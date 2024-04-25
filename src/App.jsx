186d8c57f389fe2c3c03a100ef01f46f141c1c91
MichalPresz <m.presz@wp.pl>
2024-04-25 16:57:19 +0100

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   // what if I want to do local storage? STRETCH GOAL
-  const [carrots, setCarrots] = useState(1);
-  const [cps, setCps] = useState(0); // CPS = Cookies Per Second
+  const [carrots, setCarrots] = useState(0);
+  const [cps, setCps] = useState(1); // CPS = Cookies Per Second
 
   useEffect(() => {
     // maybe you want to do some maths here for the 1000/cps etc
     // a timer to be created when the page loads to increase cookies by cps every second
     const myInterval = setInterval(() => {
       addCarrot();
-    }, 1000 / cps);
+    }, 1000);
 
     // to clean up my timer when I rerun the useEffect to i don't end up with a billion timers
     return () => {
@@ -27,27 +27,30 @@ export default function App() {
   }
 
   function buyUpgrade() {
-    setCps(cps + 1);
-    setCarrots(carrots - cps);
+    if (carrots >= 10) {
+      setCps(cps + 1);
+      setCarrots(carrots - 10);
+    }
   }
   // we triedd several options with Manny
   function resetBtn() {
     setCarrots(0);
+    setCps(0);
   }
 
   return (
     <div>
       <h1>Carrot Rain</h1>
       <button id="bunny" onClick={addCarrot}>
-        <p>I have {carrots} carrots</p>
+        <p>You have {carrots} carrots</p>
         <img className="hoverglow" src="src/assets/Bugs_Bunny.webp" />
       </button>
       <button id="carrot" onClick={buyUpgrade}>
-        <p>I get {cps} carrots per second</p>
+        <p>You get {cps} carrots per second</p>
         <img className="hoverglow" src="src/assets/carrotnobg.png" />
       </button>
       <button id="knife" onClick={resetBtn}>
-        <p> Chopp the Carrots</p>
+        <p> Chop the Carrots</p>
         <img className="hoverglow" src="src/assets/knife.png" />
       </button>
     </div>
