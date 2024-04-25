a21119dac678e2d9e9d26d2d6a3ad8a34355bd11
MichalPresz <m.presz@wp.pl>
2024-04-25 11:49:44 +0100

    button

--- src/App.jsx
+++ src/App.jsx
@@ -33,28 +33,13 @@ export default function App() {
   return (
     <div>
       <h1>Raining Carrots</h1>
-      <button onClick={addCarrot}>I am a carrot</button>
+      <button onClick={addCarrot}>
+        {" "}
+        <img src="src/assets/Bugs_Bunny.webp" alt="bunny" />
+      </button>
       <button onClick={buyUpgrade}>Carrot Rain</button>
       <p>I have {carrots} carrots</p>
       <p>I get {cps} carrots per second</p>
     </div>
   );
 }
-
-<body>
-<div>
-  <p>Carrots: <span id="cookiesSpan">0</span></p>
-</div>
-<div>
-  <p>Carrots Per Second: <span id="cpsSpan">0</span></p>
-</div>
-<div>
-  <button id="cookieBtn"><img src="./Bugs_Bunny.webp" /></button>
-</div>
-<div>
-  <p><button id="upgradeBtn">Raining Carrots Upgrade</button></p>
-</div>
-<div>
-  <p><button id="restartBtn">Restart</button></p>
-</div>
-</body>
